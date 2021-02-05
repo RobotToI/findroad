38f5296d0b290dca4e5704c921b31fe86875fa93
NickRobbot <trotsyuk2001@gmail.com>
2021-02-05 21:51:45 +0300

    We add: -Bootstrap -HTML Templates -Django app -About page

--- travel/views.py
+++ travel/views.py
@@ -5,3 +5,9 @@ def home(request):
     name = 'Nick'
 
     return render(request, 'home.html', {'name': name})
+
+
+def about(request):
+    name = 'Nick'
+
+    return render(request, 'about.html', {'name': name})
