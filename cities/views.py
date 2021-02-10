56374caa77ee6381146dcc8373963f7724ebe417
NickRobbot <trotsyuk2001@gmail.com>
2021-02-10 06:48:57 +0300

    Created: -List of cities -Ability to find a city by id in DB -Some HTML changes

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+import django.shortcuts
+from cities.models import City
 
-# Create your views here.
+__all__ = (
+    'home',
+)
+
+def home(request, pk=None):
+    if pk:
+        city = django.shortcuts.get_object_or_404(City, id=pk)
+        context = {'object': city}
+        return django.shortcuts.render(request, 'cities/detail.html', context)
+
+    qs = City.objects.all()
+    context = {'objects_list':qs}
+    return django.shortcuts.render(request, 'cities/home.html', context)
